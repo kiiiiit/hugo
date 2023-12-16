36b7e66d29327d455d95b6ab4d3287e40ee62353
Sergey Dedov <s@dedov.ws>
2023-12-16 14:55:54 +0300

    Update by BoltAI

--- my2git.sh
+++ my2git.sh
@@ -1,4 +1,15 @@
-git pull
+#!/bin/bash
+
+# Скрипт для обновления локального репозитория Git с помощью команд git pull, git add, git commit и git push.
+
+# Получение последних изменений из удаленного репозитория
+git pull origin master
+
+# Добавление всех измененных файлов в индекс
 git add .
-git commit -m "Update"
-git push
+
+# Фиксация изменений с описанием "Update" и указанием автора
+git commit -m "Update by BoltAI"
+
+# Отправка локальных изменений на удаленный репозиторий в ветку master
+git push origin master
