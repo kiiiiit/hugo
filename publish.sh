615f10f72da1db32df66997b5b7666707a7e2226
Sergey Dedov <s@dedov.ws>
2023-12-20 13:59:55 +0300

    rebuilding site среда, 20 декабря 2023 г. 13:59:55 (MSK)

--- publish.sh
+++ publish.sh
@@ -1,1 +1,17 @@
-git add . && git commit -m "dd" && git push
+#!/bin/sh
+
+set -e
+
+printf "\033[0;32mДеплой на Github!..\033[0m\n"
+
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+echo ""
+echo ""
+echo "Коммитим изменения в $(pwd)"
+git add .
+git commit -m "$msg"
+git push origin
